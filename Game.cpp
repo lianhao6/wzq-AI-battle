72e7cb8a27cfc92e494c0eaf2ad6e7f6b6f86ed2
lianhao6 <156403882+lianhao6@users.noreply.github.com>
2024-07-15 20:41:45 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -33,23 +33,17 @@ public:
 
     void playerMove() {
         int x, y;
-        while(1)
-        {
-        cin >> x >> y;
-        if( x<SIZE && x>=0 && y<SIZE && y>=0 && board[x][y] == 0)
-        // to do 要判断x,y,是否合法, 判断合法在进入下一步
-        {
-        makeMove(x, y);
-        check(x, y);
-        switchPlayer();
-        break;
+        while(1) {
+            cin >> x >> y;
+            if( x<SIZE && x>=0 && y<SIZE && y>=0 && board[x][y] == 0) // to do 要判断x,y,是否合法, 判断合法在进入下一步
+            {
+                makeMove(x, y);
+                check(x, y);
+                break;
+            }
         }
-         
-        
-        }
-         
-            
     }
+
     void aiMove() {
         int x, y;
         // to do 要判断x,y,是否合法, 判断合法在进入下一步
